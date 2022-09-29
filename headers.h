c3677e15cb94375797890218defb9945113f4ec8
max-hagi <msavehilaghi99@gmail.com>
2022-09-28 20:19:11 -0400

    Fixed sequencing of child and parent code and concatenation bugs

--- headers.h
+++ headers.h
@@ -6,6 +6,7 @@
 #include "unistd.h"
 #include "sys/wait.h"
 #include <string.h>
+#include "signal.h"
 
 
 #endif
--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include "headers.h"
-#define STRSIZE 20
+#define STRSIZE 50
 
 int main(int argc, char** argv) {
+
+    if (argc != 4) { //Make sure there are only 3 command line arguments passed
+        printf("Please include only 3 arguments\n");
+        exit(1);
+    }
+
     char* X = argv[1];
     char* Y = argv[2];
     char* Z = argv[3];
     pid_t pid;
-    int status;
     int port[2];
     char str [STRSIZE];
+    char temp [STRSIZE]; //Temporary holder for strings read from pipe before concatenation
 
     if (pipe(port) < 0) {
         perror("Pipe error\n");
@@ -24,13 +30,16 @@ int main(int argc, char** argv) {
 
     if (pid > 0) { //Parent process
         printf("parent (PID %d): created child (PID %d)\n", getpid(), pid);
-        read(port[0], &str, strlen(str)); //Reading X from the pipe, will wait until there is something to read FIXME get entire string to get read
-        printf("parent (PID %d): read from pipe %s\n", getpid(), str);
+        read(port[0], &str, sizeof (str));
+        printf("parent (PID %d): read from pipe \"%s\"\n", getpid(), str);
         printf("parent (PID %d): received Y = \"%s\"\n", getpid(), Y);
-        strcat(str, Y); //Concatenating read string and Y
+        strcpy(temp, str); //Copy read string to temporary holder
+        strcat(str, " ");
+        printf("parent (PID %d): \"%s\" + Y = \"%s\"\n", getpid(), temp, strcat(str,Y));
         printf("parent (PID %d): writing into pipe \"%s\"\n", getpid(), str);
-        write(port[1], str, strlen(str)); //Writing concatenated strings to pipe
-        read(port[0], &str, strlen(str)); //Will wait until there is something to read
+        write(port[1], str, sizeof (str)); //Writing concatenated strings to pipe
+        sleep(1);
+        read(port[0], &str, sizeof (str)); //Will wait until there is something to read
         printf("parent (PID %d): read from pipe \"%s\"\n", getpid(), str);
         printf("parent (PID %d): all tasks completed\n", getpid());
     }
@@ -38,14 +47,16 @@ int main(int argc, char** argv) {
     if (pid == 0) { //Child process
         printf("child (PID %d): received X = \"%s\"\n", getpid(), X);
         printf("child (PID %d): writing \"%s\" into pipe\n", getpid(), X);
-        write(port[1], X, strlen(X)); //Writing X to the pipe
-        //FIXME Make child wait for parent to read
-        read(port[0], &str, strlen(str)); //Will wait until there is something to read
+        write(port[1], X, sizeof (X)); //Writing X to the pipe
+        sleep(1);
+        read(port[0], &str, sizeof (str)); //Will wait until there is something to read
         printf("child (PID %d): read from pipe \"%s\"\n", getpid(), str);
         printf("child (PID %d): received Z = \"%s\"\n", getpid(), Z);
-        printf("child (PID %d): \"%s\" + Z = \"%s\"\n", getpid(), str, strcat(str, Z));
+        strcpy(temp, str);
+        strcat(str, " ");
+        printf("child (PID %d): \"%s\" + Z = \"%s\"\n", getpid(), temp, strcat(str, Z));
         printf("child (PID %d): writing into pipe \"%s\"\n", getpid(), str);
-        write(port[1], str, strlen(str)); //Write concatenated string into pipe
+        write(port[1], str, sizeof (str)); //Write concatenated string into pipe
         printf("child (PID %d): all tasks completed\n", getpid());
     }
     return 0;
